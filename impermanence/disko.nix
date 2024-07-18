12042b74225509fbdd186b4f42e93bbf919c9c96
caio86 <caioluiz86@gmail.com>
2024-07-18 12:41:24 -0300

    change volume group name

--- impermanence/disko.nix
+++ impermanence/disko.nix
@@ -15,7 +15,7 @@
           };
           esp = {
             name = "ESP";
-            size = "500M";
+            size = "1G";
             type = "EF00";
             content = {
               type = "filesystem";
@@ -35,7 +35,7 @@
             size = "100%";
             content = {
               type = "lvm_pv";
-              vg = "root_vg";
+              vg = "vg0";
             };
           };
         };
